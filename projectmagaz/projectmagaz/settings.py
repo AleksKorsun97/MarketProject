43eea2b59c14676c90c269106b75abdcd6b225af
Aleks <alek_sey7777@bigmir.net>
2023-03-07 20:03:17 +0200

    setings for deploy

--- projectmagaz/projectmagaz/settings.py
+++ projectmagaz/projectmagaz/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r!z*i%x3flw2)*(sy0ph-m2=z+z17em%vj0krk^vo1rjne*m+)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
